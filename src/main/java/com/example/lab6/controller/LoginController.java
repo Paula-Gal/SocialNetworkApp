ed498249c3187ca9efe5dcda8e37f15ccd00f655
Paula Gal <galpaullla@gmail.com>
2021-12-22 20:18:38 +0200

    Hash password

--- src/main/java/com/example/lab6/controller/LoginController.java
+++ src/main/java/com/example/lab6/controller/LoginController.java
@@ -9,11 +9,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
@@ -21,8 +24,16 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.EventListener;
+import java.util.ResourceBundle;
 
 public class LoginController {
+
+    public CheckBox hidePasswordCheckbox;
+    public TextField passwordShowTextField;
+    Stage stage;
+    boolean isHiddenPass = true;
 
     public Label loginLabel;
     public AnchorPane anchorPanePurple;
@@ -31,6 +42,7 @@ public class LoginController {
     public ImageView passwordIcon;
     public TextField emailTextField;
     public TextField passwordTextField;
+
     UserService userService;
     FriendshipService friendshipService;
     MessageService messageService;
@@ -40,24 +52,16 @@ public class LoginController {
     @FXML
     private TextField loginField;
 
-    public void initialize(){
-      logo.setImage(new Image("/images/icons/social.png"));
-      emailIcon.setImage(new Image("/images/user.png"));
-      passwordIcon.setImage(new Image("/images/padlock.png"));
+    public void initialize() {
+        logo.setImage(new Image("/images/icons/social.png"));
+        emailIcon.setImage(new Image("/images/user.png"));
+        passwordIcon.setImage(new Image("/images/padlock.png"));
+        passwordShowTextField.setVisible(false);
     }
 
-    @FXML
-    public void handleLogin(){
-        email = String.valueOf(emailTextField.getText());
-        if(userService.exists(email) == null)
-            MessageAlert.showErrorMessage(null, "The account do not exists");
-        else
-            showUserDialog(email);
-    }
-
-    public void showUserDialog(String id){
+    public void showUserDialog(String id) {
         // create a new stage for the popup dialog.
-        try{
+        try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("/views/homeView.fxml"));
 
@@ -71,18 +75,17 @@ public class LoginController {
             Scene scene = new Scene(root);
             dialogStage.setScene(scene);
 
-            UserController userViewController  = loader.getController();
-            userViewController.setServices(userService, friendshipService, friendRequestService,dialogStage, id);
-
+            UserController userViewController = loader.getController();
+            userViewController.setServices(userService, friendshipService, friendRequestService, dialogStage, id);
 
             dialogStage.show();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
-    public void showCreateAccountDialog(User user){
+
+    public void showCreateAccountDialog(User user) {
         try {
 
             // create a new stage for the popup dialog.
@@ -99,30 +102,31 @@ public class LoginController {
             Scene scene = new Scene(root);
             dialogStage.setScene(scene);
 
-            AccountController accountViewController  = loader.getController();
+            AccountController accountViewController = loader.getController();
             accountViewController.setService(userService, friendRequestService, dialogStage);
 
             dialogStage.show();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     @FXML
-    public void handleCreateAccountDialog(MouseEvent ev){
+    public void handleCreateAccountDialog(MouseEvent ev) {
         showCreateAccountDialog(null);
 
     }
+
     public void loginLabel(InputMethodEvent inputMethodEvent) {
     }
 
-    public void setServices(UserService service, FriendshipService fservice, MessageService messageService, FriendRequestService friendRequestService){
+    public void setServices(UserService service, FriendshipService fservice, MessageService messageService, FriendRequestService friendRequestService, Stage stage) {
 
-        this.userService=service;
+        this.userService = service;
         this.friendshipService = fservice;
         this.messageService = messageService;
         this.friendRequestService = friendRequestService;
+        this.stage = stage;
     }
 
     public void onHandleUsers() {
@@ -144,21 +148,38 @@ public class LoginController {
             Scene scene = new Scene(root);
             dialogStage.setScene(scene);
 
-            AllUsersController allUsersController  = loader.getController();
+            AllUsersController allUsersController = loader.getController();
             allUsersController.setService(userService, dialogStage);
 
             dialogStage.show();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void viewpass() {
+        if(hidePasswordCheckbox.isSelected()) {
+            passwordTextField.setVisible(false);
+            passwordShowTextField.setVisible(true);
+            passwordShowTextField.setText(passwordTextField.getText());
+        }
+        else {
+            passwordTextField.setVisible(true);
+            passwordShowTextField.setVisible(false);
+            passwordTextField.setText(passwordShowTextField.getText());
         }
     }
 
     public void onHandleLogin(ActionEvent actionEvent) {
         email = String.valueOf(emailTextField.getText());
-        if(userService.exists(email) == null)
+        if (userService.exists(email) == null)
             MessageAlert.showErrorMessage(null, "The account do not exists");
-        else
+        if (!userService.checkPassword(passwordTextField.getText(), email))
+            MessageAlert.showErrorMessage(null, "The password is incorrect!");
+        else {
             showUserDialog(email);
+            stage.close();
+        }
     }
 }
